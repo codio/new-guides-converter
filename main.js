7dd5a05629f1ab91f89915bef2071be5293fe6c1
Maxim Kraev <maxim.kraev@gmail.com>
2015-03-02 14:35:19 +0300

    converter was added

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var DEFAULT_PATH = '/home/codio/workspace';
+var GUIDES_PATH = '/.guides/';
+var oldGuides = require('./lib/old');
+var newGuides = require('./lib/new');
+var converter = require('./lib/converter');
+
+console.log('Start');
+var path = process.env.CODIO_PROJECT_PATH || DEFAULT_PATH;
+oldGuides.load(path + GUIDES_PATH + 'sections.md').then(function (jsonStructure) {
+    console.log('Coverting...');
+    var metadata = converter(jsonStructure, newGuides);
+    return metadata;
+}).then(function (metadata) {
+    console.log('Saving...');
+    return newGuides.save(metadata, path);
+}).then(function () {
+    oldGuides.cleanup(path + GUIDES_PATH + 'sections.md');
+});
